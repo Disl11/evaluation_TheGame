44d9a4e8fb80fcc50e0dcdea4deb890245ea2835
Brian Farnier <brian.pro1993@gmail.com>
2025-10-01 16:12:50 +0200

    nettoyage + backup

--- backup/gameEngine.php
+++ backup/gameEngine.php
@@ -7,13 +7,24 @@ class GameEngine
 
     public function addCombattant(Personnage ...$personnage): void
     {
+
+        // une boucle forach sur les personage reçu dans le tableau dome du tonnere
         foreach ($personnage as $p) {
             $this->domeDuTonnere[] = $p;
         }
+
+        //ajouter un combattant max 2 combattant ( premier parti )
+        // if (count($this->domeDuTonnere) < 2) {
+        //     $this->domeDuTonnere[] = $p;
+        // } else {
+        //     echo "Maximun de combattant dans le dome du tonnere <br>";
+        // }
+
     }
 
     public function start(): void
     {
+        //lancer le combat 
         $this->tourDeJeu();
     }
 
@@ -21,7 +32,11 @@ class GameEngine
     public function tourDeJeu(): void
     {
 
-        echo "Debut du  Battle  !!! <br> ";
+        // avoir 2 warrior 
+        // rucupere getJoueur() pour idattaquant et idcible
+        //effectuer des actions a chaque tour
+
+        echo  "Le Batlle est fini !";
 
         while (!$this->fin()) {
 
@@ -39,25 +54,44 @@ class GameEngine
             $this->nettoyerMort();
         }
 
-        echo  "Le Batlle est fini ! <br>";
-
         if ($this->domeDuTonnere === 1) {
             echo "il y pas de vainqeur";
         } else {
             $winner = reset($this->domeDuTonnere);
             echo " Le vainqeur du battle est : " . $winner->getNom();
         }
+
+
+
+
+        // ================ function tourDeJeu uniquement pour 1 vs 1 =============================
+        // $attaquant = $this->domeDuTonnere[0];
+        // $cible = $this->domeDuTonnere[1];
+        // $attaquant->attaquer($cible);
+
+        // echo $attaquant->getNom() . " attaque " . $cible->getNom() . "<br>"
+        //     . " Point de vie restant de " . $cible->getNom() . " : " . $cible->getPv() . " <br><br>";
+
+        // if ($this->fin()) return;
+
+        // $attaquant = $this->domeDuTonnere[1];
+        // $cible = $this->domeDuTonnere[0];
+        // $attaquant->attaquer($cible);
+
+        // echo $attaquant->getNom() . " attaque " . $cible->getNom() . "<br>"
+        //     . " Point de vie restant de " . $cible->getNom() . " : " . $cible->getPv() . " <br><br>";
     }
 
 
     public function getId()
     {
-
+        // retourne un id aléatoirement
         return array_rand($this->domeDuTonnere);
     }
 
     public function getJoueur(int $id)
     {
+        // retourne le joueur de son l'id
         return $this->domeDuTonnere[$id];
     }
 
@@ -65,6 +99,7 @@ class GameEngine
 
     public function nettoyerMort(): void
     {
+        // elever un mort de l'arene  
         foreach ($this->domeDuTonnere as $key => $warrior) {
             if ($warrior->getPv() <= 0) {
                 echo $warrior->getNom() . " est eliminé de l'aréne ! <br><br>";
@@ -75,6 +110,7 @@ class GameEngine
 
     public function fin(): bool
     {
+        // retourne vrai si le combat et fini
         $this->nettoyerMort();
         return count($this->domeDuTonnere) <= 1;
     }
--- main.php
+++ main.php
@@ -18,11 +18,6 @@ $orc1 = new Orc("Nicolas");
 $orc2 = new Orc("Jean-claude");
 
 $game->addCombattant($elfe1, $humain1, $orc1, $orc2);
-
-// $game->addCombattant($elfe1 = new Elfe("Chloe"));
-// $game->addCombattant($humain1 = new Humain("Pierre"));
-// $game->addCombattant($orc1 = new Orc("Nicolas"));
-
 $game->start();
 
 
